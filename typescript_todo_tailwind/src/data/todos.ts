abc721d9849cddb0d61c146077c6297f84c573a9
kimyoda <rladygks1210@naver.com>
2024-07-20 03:06:45 +0900

    [feat] todo_list setting, data, todo.ts add, tailwindscss setting

---
+++ typescript_todo_tailwind/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Watch soccer game!👓",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Nice coding in typescript!⚒",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a nice comment!🎃",
+    completed: false,
+  },
+];
